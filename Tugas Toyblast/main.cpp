0946175884f4d3f0dc79d0f0e7cc782d2ae75136
MSI <MSI@DESKTOP-4KJ89N1>
2022-03-17 10:50:35 +0700

    Add project files.

---
+++ Tugas Toyblast/main.cpp
@@ -0,0 +1,22 @@
+#include "Board.h"
+
+int main()
+{
+	int x, y;
+	Board board;
+	char option = 'y';
+
+	board.printBoard();
+
+	while (option != 'n')
+	{
+		cout << endl << "Use Tile on Coordinate : ";
+		cout << endl << "[X] : "; cin >> x;
+		cout << "[Y] : "; cin >> y;
+		board.use(x, y);
+		cout << endl;
+		board.printBoard();
+		cout << endl;
+		cout << "Again ?"; cin >> option;
+	}
+}
